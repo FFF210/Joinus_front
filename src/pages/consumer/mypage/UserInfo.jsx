78ab64f4e64b39965ab7e0d1a7f8703c38a37c7a
jisung <projjang200@naver.com>
2025-12-08 12:30:21 +0900

    마이페이지 및 제안 수정

--- src/pages/consumer/mypage/UserInfo.jsx
+++ src/pages/consumer/mypage/UserInfo.jsx
@@ -1,13 +1,32 @@
 import { Card, CardHeader, CardBody, CardTitle, CardText } from "reactstrap";
 import '../../../css/mypage/UserInfo.css'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { myAxios } from "../../../config";
 
 export default function UserInfo() {
+  const [username] = useParams('1');
+  const [member, setMember] = useState({username:username, grade:'', pointBalance:'', name:''});
+
+  const getUserInfo = ()=>{
+    myAxios().get(`/consumerInfo=${username}`)
+    .then(res=>{
+      console.log(res)
+      setMember(res.data)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+  useEffect(()=>{
+    getUserInfo();
+  }, [])
   return (
     <>
       <Card style={{ width:"860px" }}>
         <CardHeader className="CardHeaderCss">
           <div style={{ marginTop:"5px", color:"#2D81EF", fontSize:"16px" }}>
-            최지성님 환영합니다.
+            {member.name} 환영합니다.
           </div>
         </CardHeader>
 
@@ -18,12 +37,12 @@ export default function UserInfo() {
             
             {/* 아이디 */}
             <CardTitle tag="h5" style={{ fontSize:"12px", marginBottom:"8px" }}>
-              아이디 : projjang2000
+              아이디 : {member.username}
             </CardTitle>
 
             {/* 회원 등급 */}
             <CardText style={{ fontSize:"12px", margin: 0 }}>
-              회원 등급 : Diamond
+              회원 등급 : {member.grade}
             </CardText>
 
           </div>
@@ -31,7 +50,7 @@ export default function UserInfo() {
           {/* 오른쪽 영역 : 보유 포인트 */}
           <div style={{ textAlign: "right", minWidth: "150px" }}>
             <div style={{ fontSize:"12px" }}>
-              보유 포인트 : 25,000P
+              보유 포인트 : {member.pointBalance}+P
             </div>
           </div>
         </CardBody>
